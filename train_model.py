75dbf02399641aaa7d76aeea17e99dc1db70fe53
seyliii <koubaailyes2@gmail.com>
2025-11-12 09:35:23 +0100

    train_model modife
    fichier txt

--- train_model.py
+++ train_model.py
@@ -1,5 +1,5 @@
 def train_model(train_X, train_y, test_X, model):
-    """entraine un modele ML de classification et retourne des predictions sur
+    """entraine un modele ML de classification modfie et retourne des predictions sur
     le dataset de test"""
     model.fit(train_X,train_y) 
     prediction=model.predict(test_X)
