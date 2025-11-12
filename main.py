0b27119d1a3beca41b281f64722644e2a8343d00
seyliii <koubaailyes2@gmail.com>
2025-11-12 09:58:13 +0100

    modife main

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sklearn import svm  
 from train_model import train_model
 from preprocess_data import preprocess_data
-
+#Hassoul
 iris = pd.read_csv("Iris.csv") #load the dataset
 test_size = 0.3 # the attribute test_size=0.3 to use for splitting the data 
 				#into 70% for train and 30% for test
